fcfd1f959c7fba9fb13333969235e42eefb614ff
martin-centurion <martinalejandrocenturion@gmail.com>
2023-04-06 17:28:42 -0300

    creando espacio de trabajo

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from '../Login/Login';
+
+function App() {
+  return (
+    <div>
+        <h1>Crazy Nails</h1>
+        <Login />
+    </div>
+  )
+}
+
+export default App;
