fd7c7ba8085d0da0fd891f4902dc78f7b5326b14
martin-centurion <martinalejandrocenturion@gmail.com>
2023-04-07 17:48:13 -0300

    estilos navbar

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -9,14 +9,34 @@ function ItemList() {
         <div className='servicios__content'>
             <div className='servicios__content-title'>
                     <h3>Manicura</h3>
-                    <p>20% Off comprando desde App</p>
+                    <p>Premium - 20% Off comprando desde App.</p>
             </div>
             <div className='servicios__content-img'>
 
             </div>
-            
+            <Button>Reservar <BiShoppingBag /></Button>
         </div>
-        <Button>Reservar <BiShoppingBag /></Button>
+        <div className='servicios__content'>
+            <div className='servicios__content-title'>
+                    <h3>Pedicure</h3>
+                    <p>Con crema de almendras.</p>
+            </div>
+            <div className='servicios__content-img'>
+
+            </div>
+            <Button>Reservar <BiShoppingBag /></Button>
+        </div>
+        <div className='servicios__content'>
+            <div className='servicios__content-title'>
+                    <h3>Esmaltado</h3>
+                    <p>con decoración.</p>
+            </div>
+            <div className='servicios__content-img'>
+
+            </div>
+            <Button>Reservar <BiShoppingBag /></Button>
+        </div>
+        
     </div>
   )
 }
