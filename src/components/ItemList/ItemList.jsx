fd52e5a724b8c77a70ef082fc8f30e5ade29bb2c
martin-centurion <martinalejandrocenturion@gmail.com>
2023-04-06 23:13:46 -0300

    agg item list

---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.css';
+import Button from '../Button/Button';
+
+function ItemList() {
+  return (
+    <div className='servicios container'>
+        <div className='servicios__content'>
+            <div className='servicios__content-title'>
+                    <h3>Manicura</h3>
+            </div>
+            <div className='servicios__content-img'>
+
+            </div>
+            
+        </div>
+        <Button />
+    </div>
+  )
+}
+
+export default ItemList;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
+import ItemList from '../ItemList/ItemList';
 
 function ItemListContainer() {
   return (
     <div className='container'>
         <NavBar />
+        <ItemList />
     </div>
   )
 }
